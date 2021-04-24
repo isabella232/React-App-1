5608733ac615025d7431874ad1fc4a89646f6cc8
IvanMobile <51763218+MobileTV@users.noreply.github.com>
2021-04-24 19:25:09 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlsWebpackPlugin = require('htmls-webpack-plugin')
+const HappyPack = require('happypack');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
     entry: './src/index.js',
     mode: 'production',
     module: {
         rules: [
             { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-            { test: /\.(js)$/, use: ['babel-loader'] }
+            { test: /\.(js)$/, use: ['babel-loader', 'happypack/loader'] }
         ]
     },
     output: {
@@ -18,11 +22,33 @@ module.exports = {
         port: 8080,
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html',
-            inject: true
-        }),
-
-    ]
+        //new HtmlWebpackPlugin({filename: 'index.html',template: 'src/index.html', inject: true}),
+		new HappyPack({
+			 loaders: [{
+                loader: 'babel-loader',
+                options: {cacheDirectory: true }
+            }]
+		}),
+		 new HtmlsWebpackPlugin({
+			 htmls: [{
+	            src: 'src/index.html',
+	            filename: 'index.html',
+	        }],
+		 }),
+		 new HardSourceWebpackPlugin()
+    ],
+	optimization: {
+        minimize: true,
+        minimizer: [
+		    new TerserPlugin({
+				include: /\/includes/,
+				test: /\.js(\?.*)?$/i,
+			})
+		],
+    },
+	performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    }
 }
